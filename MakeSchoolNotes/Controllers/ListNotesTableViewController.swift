f355cfa30e90c881d4248ffae069c99ddb20defd
owasim1 <omar.wasim@valpo.edu>
2017-06-24 16:07:48 -0700

    Fixed the text fields in notes

--- MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
+++ MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -10,19 +10,39 @@ import UIKit
 
 class ListNotesTableViewController: UITableViewController {
     
+    var notes = [Note]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return 10
+        return notes.count
     }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "listNotesTableViewCell", for: indexPath) as! ListNotesTableViewCell
+        let row = indexPath.row
+        let note = notes[row]
         cell.noteTitleLabel.text = "note's title"
-        cell.noteModificationTimeLabel.text = "note's modification time"
-        
+        cell.noteModificationTimeLabel.text = note.modificationTime.convertToString()
+            
         return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        if let identifier = segue.identifier{
+            if identifier == "displayNote"{
+                print("Table view cell tapped")
+        }
+            else if identifier == "addNote"{
+                print("+ button tapped")
+        }
+    }
+  }
+    
+    @IBAction func unwindToListNotesViewController(_ segue: UIStoryboardSegue){
+        
     }
     
 }
---
+++ MakeSchoolNotes/Models/Note.swift
@@ -0,0 +1,17 @@
+//
+//  New File.swift
+//  MakeSchoolNotes
+//
+//  Created by Omar Wasim on 6/23/17.
+//  Copyright © 2017 MakeSchool. All rights reserved.
+//
+
+import Foundation
+
+class Note{
+    
+    var title = ""
+    var content = ""
+    var modificationTime = Date()
+    
+}
