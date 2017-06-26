1d65cfbe3283d7a75374aae0900040ca3076b2cb
owasim1 <omar.wasim@valpo.edu>
2017-06-26 10:20:45 -0700

    Final commit. (Working)
    This is the final commit. The tutorial is complete.

--- MakeSchoolNotes/Controllers/DisplayNoteViewController.swift
+++ MakeSchoolNotes/Controllers/DisplayNoteViewController.swift
@@ -9,9 +9,39 @@
 import UIKit
 
 class DisplayNoteViewController: UIViewController {
+    var note: Note?
+    
+    @IBOutlet weak var noteContentTextView: UITextView!
+    
+    @IBOutlet weak var noteTitleTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-}
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        if let note = note {
+            noteTitleTextField.text = note.title
+            noteContentTextView.text = note.content
+        } else {
+            noteTitleTextField.text = ""
+            noteContentTextView.text = ""
+        }
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "save" {
+            // if note exists, update title and content
+            let note = self.note ?? CoreDataHelper.newNote()
+            note.title = noteTitleTextField.text ?? ""
+            note.content = noteContentTextView.text ?? ""
+            note.modificationTime = Date() as NSDate
+            CoreDataHelper.saveNote()
+        }
+    }
+    }
+    
+
--- MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
+++ MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -10,39 +10,66 @@ import UIKit
 
 class ListNotesTableViewController: UITableViewController {
     
+    var notes = [Note](){
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        notes = CoreDataHelper.retrieveNotes()
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        return 10
+        return notes.count
     }
+
+    
+    
+    @IBAction func unwindToListNotesViewController(_ segue: UIStoryboardSegue) {
+        self.notes = CoreDataHelper.retrieveNotes()
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "listNotesTableViewCell", for: indexPath) as! ListNotesTableViewCell
-        cell.noteTitleLabel.text = "note's title"
-        cell.noteModificationTimeLabel.text = "note's modification time"
+
+        let row = indexPath.row
+        let note = notes[row]
+
+        cell.noteTitleLabel.text = note.title
+
+        cell.noteModificationTimeLabel.text = note.modificationTime?.convertToString()
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            CoreDataHelper.delete(note: notes[indexPath.row])
+            notes = CoreDataHelper.retrieveNotes()
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let identifier = segue.identifier {
             if identifier == "displayNote" {
                 print("Table view cell tapped")
+                
+                // 1
+                let indexPath = tableView.indexPathForSelectedRow!
+                // 2
+                let note = notes[indexPath.row]
+                // 3
+                let displayNoteViewController = segue.destination as! DisplayNoteViewController
+                // 4
+                displayNoteViewController.note = note
+                
             } else if identifier == "addNote" {
                 print("+ button tapped")
             }
         }
     }
     
-    @IBAction func unwindToListNotesViewController(_ segue: UIStoryboardSegue) {
-        if let identifier = segue.identifier {
-            if identifier == "cancel" {
-                print("Cancel button tapped")
-            } else if identifier == "save" {
-                print("Save button tapped")
-            }
-        }
-    }
 }
 
